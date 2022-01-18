11004a6e542734b63306539e92d414b86bfa077a
nathan <nathan.cavalcante1010@gmail.com>
2022-01-17 23:52:33 -0300

    if else

---
+++ script.js
@@ -0,0 +1,26 @@
+let idade = 17
+
+if(idade >= 18){
+    console.log("parabens,voce tem 18 anos ou mais!")
+}
+else{
+    console.log("voce ainda nao tem 18 anos!")
+}
+
+let humano = true 
+
+if(idade>= 18&& humano == true){
+    
+    console.log("a condiçao deu true")
+}else{
+    console.log("a condiçao deu false")
+}
+
+let mes="maio"
+
+if(mes === "dezembro"|| mes === "janeiro"){console.log("seu aniversario é em dezembro ou janeiro")}
+
+else{
+    console.log(`seu aniversario é em ${mes} `)
+}
+
